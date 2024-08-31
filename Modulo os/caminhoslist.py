12836e33031748f84eead0fbde48d6eab0c2358c
Tainan-Rocha <tainan.estudo@gmail.com>
2024-08-31 16:45:19 -0300

    adicionando arquivo sobre lista de diretorios e arquivos

---
+++ Modulo os/caminhoslist.py
@@ -0,0 +1,39 @@
+# os.listdir para navegar em caminhos'
+
+import os
+
+# Diretorio
+caminho = os.path.join('/home', 'tainan', 'Área de Trabalho')
+
+# Lista as pastas e arquivos que possui no caminho
+for pasta in os.listdir(caminho):
+    caminho_completo_pasta = os.path.join(caminho, pasta)
+    print(pasta)
+
+    if not os.path.isdir(caminho_completo_pasta):
+        continue
+
+    for arquivos in os.listdir(caminho_completo_pasta):
+        print('  ', arquivos)
+
+
+# os.walk para navegar de caminhos de forma recursiva
+# os.walk é uma função que permite percorrer uma estrutura de diretórios de
+# maneira recursiva. Ela gera uma sequência de tuplas, onde cada tupla possui
+# três elementos: o diretório atual (root), uma lista de subdiretórios (dirs)
+# e uma lista dos arquivos do diretório atual (files).
+    
+from itertools import count
+
+counter = count()
+
+for root, dirs, files in os.walk(caminho):
+    the_counter = next(counter)
+    print(the_counter, 'Pasta atual', root)
+
+    for dir_ in dirs:
+        print('  ', the_counter, 'Dir:', dir_)
+
+    for file_ in files:
+        caminho_completo_arquivo = os.path.join(root, file_)
+        print('  ', the_counter, 'FILE:', caminho_completo_arquivo)
